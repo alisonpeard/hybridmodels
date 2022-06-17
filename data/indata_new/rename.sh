74832c1e12b0ae3a7e1be3cbaf8609509926793e
alisonpeard <alison.peard@gmail.com>
2022-06-17 12:59:05 +0100

    Delete data/indata_new directory

--- data/indata_new/rename.sh
+++
@@ -1,4 +0,0 @@
-for file in *areaOfInterest*.*; do mv "$file" "${file/*areaOfInterest*./areaOfInterest.}"; done
-for file in *hydrographyL*.*; do mv "$file" "${file/*hydrographyL*./hydrographyL.}"; done
-for file in *hydrographyA*.*; do mv "$file" "${file/*hydrographyA*./hydrographyA.}"; done
-for file in *observedEvent*.*; do mv "$file" "${file/*observedEventA*./flood.}"; done
